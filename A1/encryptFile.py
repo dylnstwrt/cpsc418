2618f20b9effb8e92d75b6e811f3daf68bf9e544
dylnstwrt <dylnstwrt@gmail.com>
2020-02-11 12:15:02 -0700

    incomplete encryptFile.py, example cipher see assignment page for more info about example

--- A1/encryptFile.py
+++ A1/encryptFile.py
@@ -12,13 +12,15 @@ class Meta:
             self.cs_password = cs_password
     
 
-def sha1_digest(byteArr):
+def sha1_digest(toHash):
     digest = hashes.Hash(hashes.SHA1(), backend = default_backend())
-    digest.update(byteArr)
-    return digest.finalize()
+    digest.update(toHash)
+    val = digest.finalize()
+    return val
 
 def keygen(password):
     long_key = sha1_digest(bytearray(password, "utf8"))
+    #long_key = sha1_digest(password)
     key = bytearray(16)
     for i in range(16):
         key[i] = long_key[i]
@@ -28,21 +30,28 @@ def main():
     
     args = Meta(str(sys.argv[1]), str(sys.argv[2]), str(sys.argv[3]))
     fd = open(args.pt_filename)
+
     byteArr = bytearray(fd.read(), "utf8")
     fd.close()
-    
-    
-    byteArr += sha1_digest(byteArr)
-    
+
+    extended_byteArr = byteArr + sha1_digest(byteArr)    
     key = keygen(args.cs_password)
     
     iv = os.urandom(16)
     output = open(args.tp_filename, "wb")
     # does this write to the file correctly
     output.write(iv)
+    padder = padding.PKCS7(128).padder()
+    padded_data = padder.update(bytes(extended_byteArr))
+    padded_data += padder.finalize()
+
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    encryptor = cipher.encryptor()
+
+    
+    ciphertext = encryptor.update(padded_data) + encryptor.finalize()
+    output.write(ciphertext)
     output.close()
     
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), default_backend())
-
 if __name__ == "__main__":
     main()
