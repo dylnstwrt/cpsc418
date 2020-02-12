a49ec492cd8d30351a5c706b937b5eb9f4b46b29
dylnstwrt <dylnstwrt@gmail.com>
2020-02-11 20:53:53 -0700

    mostly working, don't forget about date range

--- A1/modifyFile.py
+++ A1/modifyFile.py
@@ -9,7 +9,7 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 def calc_brute():
     brute = []
-    for k in range(1984, 2021): #TODO Change this depending
+    for k in range(1984,2021): #TODO Change this depending
         for i in range(12):
             for j in range(31):
                 toAdd = "%4d" % k +"%02d" % (i+1) + "%02d" % (j+1) #TODO Change this back
@@ -47,8 +47,9 @@ def main():
 
         unpadder = padding.PKCS7(128).unpadder()
         message = unpadder.update(plaintext)
+        message
         try:
-            message + unpadder.finalize()
+            message += unpadder.finalize()
         except:
             continue
         else:
@@ -58,16 +59,29 @@ def main():
     for i in range(len(possible_passwords)):
         if (str(possible_passwords[i][1]).find("FOXHOUND") != -1):
             correct_password = possible_passwords[i][0]
+            message_bytes = possible_passwords[i][1]
             break
     
     try:
-        print("Password: "+correct_password)
+        print("\nPassword: "+correct_password+"\n")
     except:
         print("No password found within bounds")
         exit(-1)
 
+    offset = len(message_bytes) - 20
+    toDecode = message_bytes[:offset]
+    toWrite = toDecode.decode('cp437')
+    print("Original Message: "+toWrite)
+    index = toWrite.find("CODE-RED")
+    if(index != -1):
+        toWrite = toWrite.replace("CODE-RED","CODE-BLUE", 10)
+        print("Modified: "+toWrite)
 
-        
+    output = open("plain.txt", "w")
+    output.write(toWrite)
+    output.close()
+
+    print("Plaintext has been written to plain.txt")
 
 if __name__ == "__main__":
     main()
