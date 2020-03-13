375b740e1f933dd77da299034364cbd22b370567
dylnstwrt <dylnstwrt@gmail.com>
2020-03-12 20:49:39 -0600

    registration mostly done

--- A2/a2/demo/client-demo.py
+++ A2/a2/demo/client-demo.py
@@ -6,6 +6,7 @@ import socket
 import sys, os
 import sympy
 import secrets
+import time
 
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends import default_backend
@@ -15,38 +16,53 @@ PORT = 65432        # The port used by the server, usually between 0 - 65535. Lo
 
 
 # Ask for username via standard input
+def main():
+    print("Please enter a username: ")
+    uname = sys.stdin.readline()
+    # encode it as bytes, and record the length
+    unamebytes = uname.encode('utf-8')
+    # convert store the length in a 4byte array in big-endian
+    unamelength = len(unamebytes).to_bytes(4, 'big')
 
-print("Please enter a username: ")
-uname = sys.stdin.readline()
-# encode it as bytes, and record the length
-unamebytes = uname.encode('utf-8')
-# convert store the length in a 4byte array in big-endian
-unamelength = len(unamebytes).to_bytes(4, 'big')
+    print("Please enter a password: ")
+    upassword = sys.stdin.readline()
+    upasswordbytes = upassword.encode('utf-8')
 
-#creates client string to be sent
-clientdata = unamelength + unamebytes
+    salt = os.urandom(16)
 
-print("Please enter a password: ")
-upassword = sys.stdin.readline()
-upasswordbytes = uname.encode('utf-8')
+    digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
+    digest.update(salt+upasswordbytes)
+    x = int.from_bytes(digest.finalize(), 'big')
 
-salt = os.urandom(16)
+    # create socket object
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as conn:
+        #connect to server
+        conn.connect((HOST, PORT))
+        
+        N = int.from_bytes(conn.recv(64), 'big')
+        g = int.from_bytes(conn.recv(64), 'big')
+        
+        v = pow(g,x,N).to_bytes(64, 'big')
+        
+        conn.sendall(b'r')
+        conn.sendall(unamelength)
+        conn.sendall(unamebytes)
+        conn.sendall(salt)
+        conn.sendall(v)
+        
+        x = None
+        
+        conn.close()
+    
+    time.sleep(0.5)
+    
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as conn:
+        #connect to server
+        conn.connect((HOST, PORT))
+        
 
-digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
-digest.update(salt+upasswordbytes)
-x = digest.finalize()
-
-# create socket object
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as conn:
-    #connect to server
-    conn.connect((HOST, PORT))
-
-    # Receive parameters from server
-    conn.send(clientdata)
-
-    server_response = conn.recv(1024)
-    print((server_response.decode('utf-8')))
-
+if __name__ == "__main__":
+    main()
 
 
 
