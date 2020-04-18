410cc6a2bbfe110d6ac0fcb2023f3499d39db03b
dylnstwrt <dylnstwrt@gmail.com>
2020-04-17 21:33:56 -0600

    server place holder

---
+++ A3/Client.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+'''
+File: Client.py
+Class: CPSC418 - Winter 2020
+Name: Dylan Stewart
+UCID: 30024193
+Assignment : 2
+Problem: 9
+'''
+
+import socket
+import sys, os
+import sympy
+import secrets
+import time
+
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
+HOST = '127.0.0.1'  # The server's hostname or IP address. This is the local host address
+PORT = 31802        # The port used by the server, usually between 0 - 65535. Lower ports may be resrved
+    
+    
+def main():
+    pass
+        
+if __name__ == "__main__":
+    main()
+#print('Received', repr(data))
